5414f9b0088d41a7b04258386922031a284ffd36
Aleksi Toivonen <aptoivonen@mail.com>
2021-08-03 18:18:40 +0300

    Add jest-html-reporter

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,14 @@
 const config = {
   // verbose: true,
+  reporters: [
+    "default",
+    [
+      "./node_modules/jest-html-reporter",
+      {
+        pageTitle: "Test Report",
+      },
+    ],
+  ],
 };
 
 module.exports = config;
