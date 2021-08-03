966d23008da2016d4d4d61db9c596644c828b5ef
Aleksi Toivonen <aptoivonen@mail.com>
2021-08-03 17:34:07 +0300

    Refactor class to conform to test specs

--- src/js/ship.js
+++ src/js/ship.js
@@ -1,22 +1,94 @@
+import _ from "lodash";
+
+function validateConnectedPositions(shipPositions) {
+  function isNorthFacing(positions) {
+    const [startX, startY] = positions[0];
+    for (let i = 0; i < positions.length; i++) {
+      const [x, y] = positions[i];
+      if (x !== startX || y !== startY - i) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  function isSouthFacing(positions) {
+    const [startX, startY] = positions[0];
+    for (let i = 0; i < positions.length; i++) {
+      const [x, y] = positions[i];
+      if (x !== startX || y !== startY + i) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  function isEastFacing(positions) {
+    const [startX, startY] = positions[0];
+    for (let i = 0; i < positions.length; i++) {
+      const [x, y] = positions[i];
+      if (x !== startX + i || y !== startY) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  function isWestFacing(positions) {
+    const [startX, startY] = positions[0];
+    for (let i = 0; i < positions.length; i++) {
+      const [x, y] = positions[i];
+      if (x !== startX - i || y !== startY) {
+        return false;
+      }
+    }
+    return true;
+  }
+  if (!Array.isArray(shipPositions) || shipPositions.length === 0) {
+    throw new TypeError("must give a valid ship positions array");
+  }
+  if (
+    !(
+      isNorthFacing(shipPositions) ||
+      isSouthFacing(shipPositions) ||
+      isEastFacing(shipPositions) ||
+      isWestFacing(shipPositions)
+    )
+  ) {
+    throw new RangeError("ship position coordinated must form a sound line");
+  }
+}
+
 class Ship {
+  #positions;
   #hits;
 
-  constructor(length) {
-    this.#hits = new Array(length).fill(false);
+  constructor(positions) {
+    validateConnectedPositions(positions);
+    this.#positions = positions;
+    this.#hits = [];
   }
 
-  get length() {
-    return this.#hits.length;
+  hit([x, y]) {
+    this.#validateHit([x, y]);
+    this.#hits.push([x, y]);
   }
 
-  hit(position) {
-    if (position >= 0 && position < this.length) {
-      this.#hits[position] = true;
-    }
+  getHits() {
+    return this.#hits;
   }
 
   isSunk() {
-    return this.#hits.every((hit) => hit);
+    return _.isEqual(new Set(this.#positions), new Set(this.#hits));
+  }
+
+  #validateHit([x, y]) {
+    const foundPosition = this.#positions.find(
+      ([posX, posY]) => posX === x && posY === y
+    );
+    if (!foundPosition) {
+      throw new RangeError("hit coordinates outside of ship");
+    }
   }
 }
 
