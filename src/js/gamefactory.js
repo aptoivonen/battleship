0fee8944c7f5bb69b0c6506b4164fdbe9564fb81
Aleksi Toivonen <aptoivonen@mail.com>
2021-08-11 12:04:46 +0300

    Fix GameFactory to new interface

--- src/js/gamefactory.js
+++ src/js/gamefactory.js
@@ -1,8 +1,9 @@
+import sample from "lodash/sample";
 import Game from "./game";
 
 class GameFactory {
   create() {
-    return new Game(Math.random);
+    return new Game(Math.random, sample);
   }
 }
 
